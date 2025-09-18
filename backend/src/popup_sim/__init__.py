4ffa8a13e020916599e41704ff1e382bbb167a3e
Jan-Hendrik Wendisch <jan-hendrik.wendisch@deutschebahn.com>
2025-09-18 21:18:58 +0200

    style: :art: add missing whitespace to the description of the backend package

--- backend/src/popup_sim/__init__.py
+++ backend/src/popup_sim/__init__.py
@@ -1,7 +1,8 @@
 """PopUp-Sim Backend - Freight rail transport simulation tool for Digital Automatic Coupler retrofit."""
 
-__version__ = "0.1.0"
-__author__ = "PopUp-Sim development team"
-__email__ = "team@datenschleuder.dev"
-__description__ = ("Backend for PopUp-Sim the freight rail transport simulation tool for Digital Automatic Coupler"
-                   "retrofit")
+__version__ = '0.1.0'
+__author__ = 'PopUp-Sim development team'
+__email__ = 'team@datenschleuder.dev'
+__description__ = (
+	'Backend for PopUp-Sim the freight rail transport simulation tool for Digital Automatic Coupler retrofit.'
+)
